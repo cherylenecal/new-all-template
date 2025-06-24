c59b3fd21f71e4dac6aeec25f4c5655ff2160cd2
cherylenecal <rehcenel@gmail.com>
2025-06-24 09:00:10 +0700

    Update home.py

--- home.py
+++ home.py
@@ -177,7 +177,6 @@ if uploaded_claim and uploaded_claim_ratio and uploaded_benefit:
 
     # Process claim ratio data
     claim_ratio_raw = pd.read_excel(uploaded_claim_ratio)
-    st.write("Kolom di claim_ratio_raw:", claim_ratio_raw.columns.tolist())
     policy_list = claim_transformed["Policy No"].unique().tolist()
     claim_ratio_filtered = claim_ratio_raw[claim_ratio_raw["Policy No"].isin(policy_list)]
     claim_ratio_unique = claim_ratio_filtered.drop_duplicates(subset="Policy No")
