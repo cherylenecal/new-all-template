e3e9634c87fb5b0b04f6fa338092b879409860f2
cherylenecal <rehcenel@gmail.com>
2025-06-24 13:12:00 +0700

    Update home.py

--- home.py
+++ home.py
@@ -657,7 +657,7 @@ if uploaded_claim and uploaded_claim_ratio and uploaded_benefit:
 
     # Section 7: Top 10 Employees by Number of Claims
     st.subheader("Top 10 Employees by Number of Claims")
-    
+    st.write("Kolom yang tersedia:", claim_transformed.columns)
     # Hitung jumlah klaim per Employee
     employee_counts = claim_transformed['Emp Name'].value_counts().reset_index()
     employee_counts.columns = ['Employee', 'Frequency']
