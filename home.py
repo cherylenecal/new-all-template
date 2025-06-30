543d769686cac7751ab362a2af846cbbc76da16c
cherylenecal <rehcenel@gmail.com>
2025-06-30 11:04:00 +0700

    Update home.py

--- home.py
+++ home.py
@@ -732,13 +732,14 @@ if uploaded_claim and uploaded_claim_ratio and uploaded_benefit:
     
         # Label sumbu Y
         ax.set_yticks(y)
-        ax.set_yticklabels(top10['Diagnosis'], fontsize=30, fontproperties=font_prop)
+        ax.set_yticklabels(top10['Diagnosis'], fontsize=label_font, fontproperties=font_prop)
     
         # Judul dan axis
         ax.set_title(f"{product}", fontsize=label_font + 4, weight='bold', fontproperties=font_prop)
         ax.set_xlabel("Value", fontsize=label_font, fontproperties=font_prop)
         ax.set_ylabel("Value", fontsize=label_font, fontproperties=font_prop)
         ax.tick_params(axis='x', labelsize=label_font)
+        ax.tick_params(axis='y', labelsize=label_font)
     
         # Legend
         ax.legend(loc='lower right', fontsize=label_font, frameon=True)
